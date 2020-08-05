3d5b4bc1c6e487eb7bcbc05a683f46146a520912
min <pakmk8@gmail.com>
2020-08-05 16:43:04 -0400

    initial starter build

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/react-portfolio", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to database.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to database.");
+    console.log(err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
