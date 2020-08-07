601289e5037fc7c46d9fca8c3bebac2fc65645b1
min <pakmk8@gmail.com>
2020-08-07 02:08:42 -0400

    updated home page

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
+import profile from "../../assets/images/profile.png";
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>This is my home page. </h1>
+      <div className="container">
+        <main>
+          <div className="jumbotron">
+            <h1>About Me</h1>
+            <div className="row">
+              <div>
+                <img
+                  src={profile}
+                  alt="Min"
+                  className="img-fluid float-left p-3"
+                />
+                <p>
+                  Hello! I'm Min and I am from Columbus, GA. I currently live in
+                  Atlanta, GA. I like to travel and to play basketball during my
+                  free time. Im also a fan of all Georgia sports teams!
+                </p>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
